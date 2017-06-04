83b1064d8c604ae2fec2bacbf2fdd66501fdb63b
Pierre-François Pirlet <pfpirlet@hotmail.com>
2017-06-04 16:45:49 +0200

    test 1

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const app = express();
 const mongo = require('mongodb').MongoClient;
 const dotenv = require('dotenv').config();
 const url = process.env.MONGOLAB_URI;
+const port = process.env.PORT || 8080;
+
+app.get('/:data', function(req,res){
+	var data = req.params.data;
+
+	mongo.connect(url, function(err,db){
+		if (err) throw err;
+
+		var collection = db.collection("urls");
+
+		var response = collection.find({
+			original_url: data
+		})
+
+		console.log(response);
+		db.close();
+	})
+})
