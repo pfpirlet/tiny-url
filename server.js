8f8fe4e673cec89e0fdc22484fd9b69c84cae2c2
Pierre-François Pirlet <pfpirlet@hotmail.com>
2017-06-05 16:53:25 +0200

    last index problem

--- server.js
+++ server.js
@@ -10,22 +10,40 @@ function getMyIndex(){
 	mongo.connect(url, function(err,db){
 		if (err) throw err;
 		var collection = db.collection("urls");
-		var myIndex = collection.find().limit(1).sort({$natural:-1}).toArray(function(err, data){
+		var myIndex = collection.find().sort({_id:-1}).limit(1).toArray(function(err, data){
 			console.log(data);
 			var index = url.lastIndexOf("/");
 			var lastIndex = parseInt(data[0]["short_url"].match(/[^\/]+$/));
 			console.log("lastIndex: " + lastIndex);
-			return lastIndex
+			return lastIndex;
 		});
 		db.close();
 	});	
-	return myIndex + 1
+	return myIndex + 1;
+}
+
+function addURL(newIndex, data){
+	mongo.connect(url, function(err,db){
+		if (err) throw err;
+		var collection = db.collection("urls");
+		var shortUrl = "https://tiny-url-pfp.herokuapp.com/" + newIndex;
+		var newEntry = { "original_url": data, "short_url": shortUrl }
+
+		collection.insert(newEntry, function(err, data){
+			if (err) throw err;
+			console.log(data);
+		})
+		db.close();
+		console.log("New Entry: " + JSON.stringify(newEntry));
+		return JSON.stringify(newEntry);
+	});	
 }
 
 app.get('/new/:data', function(req,res){
 	var data = req.params.data;
-	var newIndex = getMyIndex();
-	res.send("newIndex: " + newIndex);
+	var newIndex = getMyIndex(); // pose problème ici: ne semble pas prendre la dernière entrée.
+	var newEntry2 = addURL(newIndex, data);
+	res.send("New Entry: " + newEntry2);
 
 
 		//collection.find({original_url: data}).toArray(function(err, results){ //affiche ce qui correspond à data
