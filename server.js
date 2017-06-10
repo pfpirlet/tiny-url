dcca8e56e9539f709f500c61d53b266bce768168
Pierre-François Pirlet <pfpirlet@hotmail.com>
2017-06-10 19:43:52 +0200

    new entry v1 ok

--- server.js
+++ server.js
@@ -1,57 +1,35 @@
 const express = require('express');
 const app = express();
-const mongo = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const url = process.env.MONGOLAB_URI;
 const port = process.env.PORT || 8080;
 var myIndex = 0;
 
-function getMyIndex(){
-	mongo.connect(url, function(err,db){
-		if (err) throw err;
-		var collection = db.collection("urls");
-		var myIndex = collection.find().sort({_id:-1}).limit(1).toArray(function(err, data){
-			console.log(data);
-			var index = url.lastIndexOf("/");
-			var lastIndex = parseInt(data[0]["short_url"].match(/[^\/]+$/));
-			console.log("lastIndex: " + lastIndex);
-			return lastIndex;
-		});
-		db.close();
-	});	
-	return myIndex + 1;
-}
-
-function addURL(newIndex, data){
-	mongo.connect(url, function(err,db){
-		if (err) throw err;
-		var collection = db.collection("urls");
-		var shortUrl = "https://tiny-url-pfp.herokuapp.com/" + newIndex;
-		var newEntry = { "original_url": data, "short_url": shortUrl }
-
-		collection.insert(newEntry, function(err, data){
-			if (err) throw err;
-			console.log(data);
-		})
-		db.close();
-		console.log("New Entry: " + JSON.stringify(newEntry));
-		return JSON.stringify(newEntry);
-	});	
-}
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+var urlSchema = new mongoose.Schema({original_url:String,short_url:String});
+var urlModel = db.model('urls', urlSchema);
 
 app.get('/new/:data', function(req,res){
 	var data = req.params.data;
-	var newIndex = getMyIndex(); // pose problème ici: ne semble pas prendre la dernière entrée.
-	var newEntry2 = addURL(newIndex, data);
-	res.send("New Entry: " + newEntry2);
+	var index = 0;
 
-
-		//collection.find({original_url: data}).toArray(function(err, results){ //affiche ce qui correspond à data
-			//res.send(results);
-		//})
-
-		
-})
+	urlModel.count({}, function(err , count){
+  		var index = count;
+ 		console.log("index: " + index);
+ 		var newEntry = new urlModel({ original_url: data, short_url: "https://tiny-url-pfp.herokuapp.com/" + index});
+		newEntry.save(function (err, data) {
+  			if (err) {
+    			console.log(err);
+  			} else {
+    			console.log("New entry added. Data :" + data);
+    			res.send(data["short_url"]);
+  			}
+		});
+	});
+});
 
 app.listen(port, function () {
   console.log('App listening on port: ' + port)
